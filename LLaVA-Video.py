888ab123406465b478b64ce05f1c64bb270099b1
Ditchfit <101690305+Tom0267@users.noreply.github.com>
2025-05-02 17:00:47 +0100

    Update LLaVA-Video.py to use gpu

--- LLaVA-Video.py
+++ LLaVA-Video.py
@@ -1,5 +1,17 @@
 # Load model directly
 from transformers import AutoProcessor, AutoModelForCausalLM
+import torch
 
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
 processor = AutoProcessor.from_pretrained("lmms-lab/LLaVA-Video-7B-Qwen2")
 model = AutoModelForCausalLM.from_pretrained("lmms-lab/LLaVA-Video-7B-Qwen2")
+model = model.to(device)
+
+# device = "cuda:0" if torch.cuda.is_available() else "cpu"
+# sentence  = 'Hello World!'
+# tokenizer = AutoTokenizer.from_pretrained('bert-large-uncased')
+# model     = BertModel.from_pretrained('bert-large-uncased')
+
+# inputs    = tokenizer(sentence, return_tensors="pt").to(device)
+# model     = model.to(device)
+# outputs   = model(**inputs)
